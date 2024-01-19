3a9f35490847524085d4ba389cd9b4fd48c80471
drashtantsolanki <sdrashtant@gmail.com>
2024-01-19 18:44:41 +0100

    layout-design

---
+++ src/app/Containers/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { LayoutService } from '../../Services/layout.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+  /**
+   *
+   */
+  constructor(private layoutService: LayoutService) {
+  }
+
+  onSideNavToggle() {
+    this.layoutService.triggerSideBarToggle();
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'admin', pathMatch: 'full' },
+    { path: 'admin', loadComponent: () => import('./Admin/admin.component').then(c => c.AdminComponent) },
+];
